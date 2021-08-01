9fee1bcec28da1760a39cf6b44420a4f2f7cac70
Radu Daia <radu.daia93@gmail.com>
2021-08-01 21:22:47 +0300

    Add option to make plots for all coins

--- plotter.sh
+++ plotter.sh
@@ -4,8 +4,25 @@
 DATA_FOLDER="data"
 PLOTS_FOLDER="plots"
 
+
+if [ ! -d "$DATA_FOLDER" ]
+then
+  echo "[ERROR] Input folder '$DATA_FOLDER' does not exist!"
+  exit 1
+fi
+
 mkdir -p $PLOTS_FOLDER
 # plot coin evolution over time
+
+DATA_FILES=($(ls $DATA_FOLDER | sed 's/.csv//g'))
+
+if [ ! -z $1 ]
+then
+  DATA_FILES=($1)
+fi
+
+for coin in "${DATA_FILES[@]}"
+do
 gnuplot <<- EOF
 	set datafile separator ','
 	set autoscale
@@ -18,6 +35,9 @@ gnuplot <<- EOF
 	set xlabel 'time'
 	set ylabel 'USD($)'
 	set term png
-	set output '$PLOTS_FOLDER/$1.png'
-	plot '$DATA_FOLDER/$1.csv' using 1:2 with boxes
+	set output '$PLOTS_FOLDER/$coin.png'
+	plot '$DATA_FOLDER/$coin.csv' using 1:2 with boxes
 EOF
+done
+
+
